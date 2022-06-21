925d34a2b6e34b012328b2d61894b528a4bbec4e
ggyoz <ggyoz@naver.com>
2022-06-21 17:18:05 +0900

    topics 추가

--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, HttpResponse
 import random
 
+topics = [
+{'id':1, 'title':'Routing', 'body':'Routing is ...'},
+{'id':2, 'title':'View', 'body':'View is ...'},
+{'id':3, 'title':'Model', 'body':'Model is ...'}
+]
+
 # Create your views here.
-def index(request):
+def index(request):    
+    global topics    
+    ol = ''    
+    for topic in topics:
+        ol += f'<li>{topic["title"]}</li>'        
+    
     #return HttpResponse('Welcome!')
-    return HttpResponse('<h1>Random</h1>' + str(random.random()))
+    #return HttpResponse('<h1>Random</h1>' + str(random.random()))
+    
+    return HttpResponse( ol )
 
 def create(request):
     return HttpResponse('Create')
